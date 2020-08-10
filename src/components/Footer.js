1fdea10dfc4aceef468880b65ddfd9e966a272a1
Kaleigh Spurio <kaleighastell@gmail.com>
2020-08-10 13:07:25 -0400

    added footer and made it fixed, adjusted width of project image

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,22 @@
 import React from 'react';
 
 export default function Footer() {
-    return <p>Footer</p>
+    const footerStyle = {
+        position: "fixed",
+        width: "100%",
+        bottom: 0,
+        overflow: "hidden",
+    }
+
+    return (
+        <footer className="is-size-7-mobile" style={footerStyle} >
+        <div className="has-text-centered mt-3">
+          <p className="mt-0">Kaleigh Spurio</p>
+          <a className="white-font" href="mailto:kaleighastell@gmail.com"
+            >kaleighastell@gmail.com</a
+          >
+          <p className="mb-3">(603)770-7841</p>
+        </div>
+      </footer>
+    );
 }
--- src/components/Project.js
+++ src/components/Project.js
@@ -2,51 +2,57 @@ import React from 'react';
 import ProjectData from '../utils/Projects.json';
 
 export default function Project() {
+  const imageStyle = {
+    width: '90%',
+    maxWidth: '300px',
+  };
+
   return (
     <div className="container">
       {ProjectData.map((project) => (
-        <div key={project.id} className="level columns project-div">
+        <div key={project.id} className="level columns project-div my-4">
           <div className="column is-3 is-centered">
+            <h3 className="title is-4">{project.projectName}</h3>
             <img
+              style={imageStyle}
               src={process.env.PUBLIC_URL + project.image}
               alt={project.projectName}
             />
           </div>
           <div className="column">
-            <h3 className="title is-4">
-              {project.projectName}
-            </h3>
             <div className="columns">
               <div className="column is-7">
                 <p className="white-font is-size-7-mobile">
                   {project.projectDescription}
                 </p>
+                <div className="columns">
+                  <a
+                    className="column is-6 mt-4"
+                    href={project.deployedLink}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    View the app
+                  </a>
+                  <a
+                    className="column is-6 mt-4"
+                    href={project.repoLink}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    View the repository
+                  </a>
+                </div>
               </div>
               <div className="column is-5">
                 <dl>
                   {project.toolsUsed.map((tool) => (
-                    <li key={tool}>{tool}</li>
+                    <li key={tool} className="is-size-7-mobile">
+                      {tool}
+                    </li>
                   ))}
                 </dl>
               </div>
-            </div>
-            <div className="columns">
-              <a
-                className="column is-3 mt-4"
-                href={project.deployedLink}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                View the app
-              </a>
-              <a
-                className="column is-4 mt-4"
-                href={project.repoLink}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                View the repository
-              </a>
             </div>
           </div>
         </div>
@@ -54,4 +60,3 @@ export default function Project() {
     </div>
   );
 }
-
