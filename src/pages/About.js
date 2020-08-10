2479a22711e36f4b5c586e180f55bb8d4ede7b0a
Kaleigh Spurio <kaleighastell@gmail.com>
2020-08-10 10:27:00 -0400

    added content to about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,32 @@
 import React from 'react';
 
 export default function About() {
-    return <p>about</p>
+  return (
+    <body>
+      <section class="section">
+        <div class="container">
+          <h1 class="title">About</h1>
+          <p>
+            My name is Kaleigh Spurio and I am excited to delve down a career
+            path as a Full Stack Web Developer. I hold a Bachelor of Arts in
+            Teaching, and a Masters of Education which have provided me with a
+            solid background of collaboration and problem solving within the
+            field of education, and consider myself a life long learner. I
+            recently earned a certificate in Full Stack Web Development from the
+            University of New Hampshire Coding Bootcamp, where I developed
+            skills in JavaScript, HTML, CSS, Node.js, Express, SQL, MongoDB,
+            ReactJS and responsive website design.
+            <br /> <br />
+            Recognized for my excellent communication skills, quick learning and
+            attention to detail, I work well both in and out of a collaborative
+            team setting and have been a valued team member within all of my
+            past professional environments. I’m passionate about creating web
+            applications that provide a meaningful user experience and am very
+            excited to leverage my skills and in a fast-paced and collaborative
+            team of developers.
+          </p>
+        </div>
+      </section>
+    </body>
+  );
 }
