c314729230ab44b7c11676c31e204bc15cdf0615
Kaleigh Spurio <kaleighastell@gmail.com>
2020-08-11 09:41:25 -0400

    Updates

--- precache-manifest.df2d82c4b4eaf4cc07b73a81f2dbbd43.js
+++ precache-manifest.df2d82c4b4eaf4cc07b73a81f2dbbd43.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f610594fbb10dd94b97035eebcedb27b",
+    "revision": "ed72e5be60198944d67bb8c2082cbfc0",
     "url": "/react-portfolio/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-portfolio/static/css/2.0c0672e5.chunk.css"
   },
   {
-    "revision": "14600a22da48292f7c7c",
-    "url": "/react-portfolio/static/css/main.764cb25b.chunk.css"
+    "revision": "fb6792dd9e5e362cb4f7",
+    "url": "/react-portfolio/static/css/main.4d859354.chunk.css"
   },
   {
     "revision": "9bbf29e407809e22a141",
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-portfolio/static/js/2.38c1b7d4.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "14600a22da48292f7c7c",
+    "revision": "fb6792dd9e5e362cb4f7",
     "url": "/react-portfolio/static/js/main.0bc4fba8.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.0d4788552fc9aff19ce1fb3ea58a79cc.js"
+  "/react-portfolio/precache-manifest.df2d82c4b4eaf4cc07b73a81f2dbbd43.js"
 );
 
 self.addEventListener('message', (event) => {
