5002332c58bebdca8a65dce2e6ccd57c2ed2f7dd
Kaleigh Spurio <kaleighastell@gmail.com>
2020-08-10 20:11:50 -0400

    Updates

---
+++ precache-manifest.fa0f526bf25087bd334c599e3dc23cba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a85bd78f704b24dee37ea6a5b32c02ac",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "9bbf29e407809e22a141",
+    "url": "/react-portfolio/static/css/2.0c0672e5.chunk.css"
+  },
+  {
+    "revision": "403c2bc196e6a4b98328",
+    "url": "/react-portfolio/static/css/main.b83038d5.chunk.css"
+  },
+  {
+    "revision": "9bbf29e407809e22a141",
+    "url": "/react-portfolio/static/js/2.38c1b7d4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.38c1b7d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "403c2bc196e6a4b98328",
+    "url": "/react-portfolio/static/js/main.9ac31d78.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  }
+]);
